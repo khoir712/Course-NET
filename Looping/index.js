1d126d74b678c4a3909c64331b6863291272e023
khoir712 <khoiri3794@gmail.com>
2023-06-24 11:16:42 +0700

    conditional2

---
+++ Looping/index.js
@@ -0,0 +1,65 @@
+// Looping
+
+// for (var i = 1; i <= 10; i++) {
+//     console.log(i);
+//     }
+
+/**Study Case 1
+ * 
+ * Cetak angka dari 1 sampai 10 dan cek apakah bilangan genap atau ganjil
+ * 
+ * contoh
+ * var number=10;
+ * 
+ * results
+ * 1 adalah ganji
+ * 2 adalah genap
+ * 3 adalah ganji
+ *  ....
+ * 9 adalah ganji
+ * 10 adalah genap
+ * 
+ * 
+ */
+ 
+/**Study Case 2
+ * 
+ * print sebuah string menjadi huruf besar dan kecil.
+ * 
+ * contoh=nasi kotak
+ * 
+ * result
+ * NaSi kOtAk 
+ * 
+ */
+//Jawaban Case 1
+// var num = 10;
+
+// for (var i = 1; i <= num; i++) {
+//     if (i % 2 === 0){
+//         console.log(i +" adalah bilangan Genap");
+//     }else
+//     console.log(i +" adalah bilangan Ganjil");
+//     }
+
+// var str = "nasi kotak"
+//jawaban case 2
+
+// for (var i = 0; i < str.length; i++) {
+//     console.log(str[i]);
+// }
+
+// var str = "nasi kotak";
+// var out = "";
+// for (var i = 0; i < str.length; i++) {
+//   if (i % 2 === 0) {
+//     out += str[i].toUpperCase();
+//   } else {
+//     out += str[i].toLowerCase();
+//   }
+// }
+// console.log(out);
+
+
+
+
