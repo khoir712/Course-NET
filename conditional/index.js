174847e6d7fdd0ed28904aa1d6d5440e1fd3e6e9
khoir712 <khoiri3794@gmail.com>
2023-06-24 10:35:32 +0700

    conditional

---
+++ conditional/index.js
@@ -0,0 +1,70 @@
+//Conditionals
+
+/**
+ * Study Case
+ * 
+ * Cek Bilangan ganjil & Genap
+ * 
+ * Contoh
+ *  var num = 5;
+ * 
+ * Results: 
+ * 5 adalah Bilangan Ganjil
+ *  
+ */
+
+/**
+ * Study Case 2
+ * 
+ * School Grade
+ * Buatlah Codingan untuk cek Grade sekolah
+ * 
+ * Grade A : score antara 100 - 85
+ * Grade B : score antara 84 - 70
+ * Grade C : score antara 69 - 55
+ * Grade D : score antara 54 - 40
+ * Grade E : score antara 39 - 0
+ * 
+ */
+
+/** Study Case 3
+ * 
+ * Ada 3 Jenis acces : Public, Protected, Private.
+ * 
+ * Jika access kosong maka tampilkan input access :
+ * 
+ * Access Public, Jika Level dibawah 5 maka tampilkan"Publik dibawah 5"
+ * Access Public, Jika Level diatas 5 maka tampilkan"Publik diatas 5"
+ * Access Public, Jika Level diatas 5 maka tampilkan"Publik diatas 5"
+ */
+
+
+//Jawaban Case 1
+// var angka = 5;
+
+// // Memeriksa bilangan ganjil atau genap
+// if (angka % 2 === 0) {
+//   console.log(angka + " adalah bilangan genap.");
+// } else {
+//   console.log(angka + " adalah bilangan ganjil.");
+// }
+
+
+//Jawaban Case 2
+// var score = 75;
+
+// if (score >= 85 && score <= 100) {
+//     console.log("Grade A");
+// } else if (score >= 70 && score <= 84) {
+//     console.log("Grade B");
+// } else if (score >= 55 && score <= 69) {
+//     console.log("Grade C");
+// } else if (score >= 40 && score <= 54) {
+//     console.log("Grade D");
+// } else if (score >= 0 && score <= 39) {
+//     console.log("Grade E");
+// } else {
+//     console.log("Salah Ya scorenya");
+// }
+
+
---
+++ indexx.js
@@ -0,0 +1,109 @@
+// console.log("Hello World");
+
+//study case
+// Buatlah algoritma dan psoeudocode dan codingan dari keliling persegi panjang.
+
+/**
+ * Algoritma
+ * 
+ * 1. Buatlah var panjang
+ * 2. Buatlah var lebar
+ * 3. Buatlah var keliling
+ * 4. isi keliling dengan rumus 2 * panjang + 2 lebar
+ * 5. cetak keliling
+ * 
+ * Psoeudocode
+ * Store panjang with any number
+ * store lebar with any number
+ * 
+ * store keliling with 2 MUL panjang ADD 2 MUL Lebar
+ * Print Keliling
+ * 
+ * 
+ */
+// codingan
+
+// var panjang= 10;
+// var lebar =5;
+
+// var keliling= 2 * panjang + 2 * lebar;
+// console.log(keliling);
+
+// study case 
+// tentukan algoritma dan psoeudocode dan codingan dari luas permukaan tabung.
+
+/**
+ * ALGORITMA
+ * 
+ * 1. tentukan rumus luas permukaan tabung
+ * 2. rumus lpt = 2 * phi * r * t + 2 * phi * r * r
+ * 3. buat var r
+ * 4. buat var t
+ * 5. buat var phi isi dengan 3.14
+ * 6. isi lpt = 2*phi*r*t + 2*phi *r*r
+ * 7. cetak lpt
+ * 
+ * PSEOUDOCODE
+ * 
+ * STORE t WITH NUMBER
+ * STORE phi WITH 3.14
+ * STORE r WITH NUMBER
+ * 
+ * store lpt with 2 MUL phi MUL r MUl t ADD 2 MUL phi MUL r MUL t
+ * PRINT lpt
+ */
+
+//Codingan
+// var t = 10;
+// var r = 5;
+// var phi = 3.14;
+
+// var lpt = 2 * phi * r * r + 2 * phi * r * t;
+// console.log(lpt);
+
+// var umur = 17;
+// if (umur >= 17) {
+//     console.log("Bisa Bikin KTP");
+// } else {
+//     console.log("Belum Bisa Buat KTP");
+// }
+
+// var minuman = "AIR";
+// switch(minuman){
+//   case 'kopi':
+//      console.log("Kamu pilih kopi");
+//      break;
+// case 'teh':
+//      console.log("Kamu pilih teh");
+//      break;
+// }
+
+// var umur = 15;
+// var tinggi = 149;
+
+// if (umur < 17) {
+
+//   if (tinggi < 150) {
+//     console.log("Tidak boleh masuk");
+//   } else {
+//     console.log("Boleh masuk");
+//   }
+// }
+
+//Pertemuan Ke3 --Looping--
+// var i = 0;
+// while (i < 3){
+//     console.log("Pertemuan Ke3");
+// }
+// for
+// for ( var j = 1; j <= 10; j++ ) {
+//     console.log(j);
+//     }
+
+// while
+// var j = 1;
+// while ( j <= 10 ) {
+//     console.log(j);
+//         j++;
+//     }
+
