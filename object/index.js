56a999426b2945395afcc74c14c9800caaaeeaa6
khoir712 <khoiri3794@gmail.com>
2023-06-25 15:56:54 +0700

    khoir

---
+++ object/index.js
@@ -0,0 +1,53 @@
+//OBject
+// var buah = [
+//   {
+//     jenis: "durian",
+//     harga: 75000,
+//     enak: true,
+//   },
+// ];
+
+// function changeToObject(array) {
+//   var result = {};
+//   result.jenis = array[0].jenis;
+//   result.harga = array[0].harga;
+//   result.enak = array[0].enak;
+//   return result;
+// }
+
+// console.log(changeToObject(buah));
+
+// CRUD Create, Read, Delete, Update
+
+//Array pf object
+
+// let item = [
+//   {
+//     id: 1,
+//     nama: "Apel USA",
+//     type: "Fruit",
+//     price: 1000000,
+//     stock: 5,
+//   },
+//   {
+//     id: 2,
+//     nama: "Apel Japali",
+//     type: "Fruit",
+//     price: 1000000,
+//     stock: 5,
+//   },
+//   {
+//     id: 1,
+//     nama: "Apel Korean",
+//     type: "Fruit",
+//     price: 1000000,
+//     stock: 5,
+//   },
+// ];
+
+// function shoiTems() {
+//     console.log("item list :");
+//     for (let i = 0; i < DataTransferItemList.length; i++) {
+//         console.log(
+//             items[i].id + " : " + items[i].
+//         )
